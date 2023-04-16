730ba96f33bfcdefc3ca2e5cf5105d7a2862232f
Paulina Ester Egvarth Rehbein <68613088+PaulinaEster@users.noreply.github.com>
2023-04-15 22:57:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,87 +1,66 @@
-let respostas = [];
+let respostas = '';
 let formularioValido = false;
 const forms = document.querySelectorAll('.needs-validation');
 
-document.getElementById('submitForm').addEventListener('click', validarForm);
+document.getElementById('submitForm').addEventListener('click', calculaResultado);
 
-function validarForm(){
-
-  Array.from(forms).forEach(form => {
-    form.addEventListener('click', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-        console.log(form.checkValidity(), "invalido")
-        form.classList.add('was-validated');
-      } else {
-        calculaResultado();
-      }
-    }, false);
+function calculaResultado() {
+  respostas = '';
+  let aux = document.querySelectorAll('.formulario');
+  aux.forEach((node, index) => {
+    gerarResposta(node, 'Tecnologia', 'https://olhardigital.com.br/?amp', ' https://g1.globo.com/tecnologia/');
+    gerarResposta(node, 'Economia', 'https://www.infomoney.com.br/ ', 'https://g1.globo.com/economia/negocios/ ');
+    gerarResposta(node, 'Saúde', 'http://conselho.saude.gov.br/ultimas-noticias-cns', 'https://g1.globo.com/saude/');
+    gerarResposta(node, 'Educação', 'https://exame.com/busca/Educa%C3%A7%C3%A3o%20/', 'https://g1.globo.com/educacao/ ');
+    gerarResposta(node, 'Agronegócio','https://fia.com.br/blog/agronegocio/#:~:text=O%20agroneg%C3%B3cio%20se%20refere%20a,Empresas%20agr%C3%ADcolas', 'https://www.portaldoagronegocio.com.br/' );
+    gerarResposta(node, 'Moda e Beleza', 'https://br.fashionnetwork.com/ ', 'https://g1.globo.com/pop-arte/moda-e-beleza/');
+    gerarResposta(node, 'Esporte', 'https://ge.globo.com/', 'https://ge.globo.com/' );
+    gerarResposta(node, 'Marketing', 'https://www.mundodomarketing.com.br/', 'https://g1.globo.com/economia/negocios/');
+    gerarResposta(node, 'Ciência', 'https://www.ipea.gov.br/portal/', 'https://g1.globo.com/ciencia/');
   });
-}
-
-
-function calculaResultado(){
-  Array.from(forms).forEach(form => {
-    console.log(form[0].value) // acesso ao valor
-    console.log(form[0].id) // acesso ao id da pergunta
-    console.log(form[1].value)
-    console.log(form[2].id) 
- 
-
-  });
-
-
   mostraResultado();
 }
+function gerarResposta(node, assunto, linkTecnico, linkGeral) {
+  if (node.querySelector(".pergunta").textContent.includes(assunto)) {
 
+    let opcoes = node.querySelectorAll('.form-check');
 
+    opcoes.forEach((form) => {
+      if (form.childNodes[1].checked) {
+
+        if (form.childNodes[1].id.includes('Tecnico')) {
+
+          respostas +=` 
+            <div class="alert alert-primary" role="alert">
+              ${assunto}: interresse técnico. Para saber mais sobre <a href="${linkTecnico}" target="_blank" class="alert-link">clique aqui.</a>
+            </div> `;
+        } else if (form.childNodes[1].id.includes('Geral')) {
+
+          respostas += `  
+            <div class="alert alert-warning" role="alert">
+              ${assunto}: interresse geral. Para saber mais sobre <a href="${linkGeral}" target="_blank" class="alert-link">clique aqui.</a>
+            </div>  `;
+        } 
+        // else {
+        //   // <li class="list-group-item">An item</li>
+        //   respostas +=`  
+        //   <div class="alert alert-info" role="alert">
+        //     ${assunto}: sem interesse.
+        //   </div>  `;
+        // } 
+      };
+    });
+  };
+}
 
 
 function mostraResultado() {
-  let tipoResultado = 'light';
-  let legandaResultado = 'Não foi possivel calcular o resultado';
+  let tipoResultado = respostas == '' ? 'light' : 'primary';
+  let legandaResultado = respostas == '' ? 'Não foi possivel calcular o resultado' : respostas;
   let recomendacao = 'é altamente recomendado blablabla';
 
   document.getElementById('resultado').innerHTML = `
-  <div class="card">
-    <div class="card-header">
-      Resultado: 
-    </div>
-    <div class="card-body">
-      <blockquote class="blockquote mb-0">
-        <div class="alert alert-${tipoResultado}" role="alert">
-          ${legandaResultado}
-        </div>
-        <footer class="blockquote-footer"> ${recomendacao} </footer>
-      </blockquote>
-    </div>
-  </div> `; 
+    <div class="row">
+      ${legandaResultado}
+    </div>`;
 }
-
-/*
-<div class="alert alert-primary" role="alert">
-  A simple primary alert—check it out!
-</div>
-<div class="alert alert-secondary" role="alert">
-  A simple secondary alert—check it out!
-</div>
-<div class="alert alert-success" role="alert">
-  A simple success alert—check it out!
-</div>
-<div class="alert alert-danger" role="alert">
-  A simple danger alert—check it out!
-</div>
-<div class="alert alert-warning" role="alert">
-  A simple warning alert—check it out!
-</div>
-<div class="alert alert-info" role="alert">
-  A simple info alert—check it out!
-</div>
-<div class="alert alert-light" role="alert">
-  A simple light alert—check it out!
-</div>
-<div class="alert alert-dark" role="alert">
-  A simple dark alert—check it out!
-</div>
-*/
